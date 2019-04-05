437c5559e95f9a3ab4b294a2ad2c8ab8441e5604
Wicky <wicky.akloe@gmail.com>
2019-04-05 10:46:15 +0000

    added more examples

--- scriptJqueryEffects.js
+++ scriptJqueryEffects.js
@@ -104,6 +104,49 @@ $(document).ready(function() {
 
 	$("button").mouseleave(function(){
 	$("button").removeClass("makeBorder").addClass("makeRed");
-});
+	
+	$("button").click(function() {
+	$("p").slideToggle(2000);
+	});
+  
+	// hides/shows paragraphs when either button is clicked
+	$("button").click(function() {
+		$("p").hide(2000).show(2000);
+    });
+
+    });
 
 }); 
+
+//wait until page is ready
+$(document).ready(function() {
+	//sets <a> element within paragraph  to yellow  
+	$("p").click(function(){
+		$(this).children("a").css("background-color", "yellow"); /* returns all the <a> child elements that are 
+		within this paragraph*/
+	});
+
+	// Toggle the visibility of the paragraph when a button is clicked 
+	$("button").click(function(){
+		$(this).prev().slideToggle('slow');
+	});
+
+	// Open the paragraph once the image is clicked
+	$("img").click(function() {
+		$(this).next().children("p").slideDown();
+	});
+
+	$(".card").click(function() {
+		$(this).toggleClass("highlight");	 
+	});
+
+	// All cards that are not currently selected will be hidden when `select_btn` is clicked
+	$("#select_btn").click(function() {
+		$(".card:not(.highlight)").hide();	 
+	});
+
+	// Select all cards
+	$("#all_btn").click(function(){
+		$(".card").show();	 
+	});
+});
